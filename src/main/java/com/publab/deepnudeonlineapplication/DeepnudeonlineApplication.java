bec6fa32f2141d1fb48aedaf04a48ddced643c13
Pavel Shvetsov <pshvetso@mail.ru>
2019-11-03 09:06:54 +0300

    Created application, data-schema.

---
+++ src/main/java/com/publab/deepnudeonlineapplication/DeepnudeonlineApplication.java
@@ -0,0 +1,13 @@
+package com.publab.deepnudeonlineapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepnudeonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepnudeonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publab/deepnudeonlineapplication/DeepnudeonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publab.deepnudeonlineapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepnudeonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
