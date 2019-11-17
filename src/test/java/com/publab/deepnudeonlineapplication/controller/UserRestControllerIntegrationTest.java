c0c061da16e412cf4b28be942999bcfef83934d3
Pavel Shvetsov <pshvetso@mail.ru>
2019-11-17 12:52:53 +0300

    H2 tests dependency, UserRestController test.

--- src/test/java/com/publab/deepnudeonlineapplication/controller/UserRestControllerIntegrationTest.java
+++ src/test/java/com/publab/deepnudeonlineapplication/controller/UserRestControllerIntegrationTest.java
@@ -1,13 +1,24 @@
 package com.publab.deepnudeonlineapplication.controller;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import com.publab.deepnudeonlineapplication.DeepnudeonlineApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
         classes = DeepnudeonlineApplication.class)
@@ -15,4 +26,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(
         locations = "classpath:application-integrationtest.properties")
 public class UserRestControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void whenInsertingUser_thenUserIsCreated() throws Exception {
+        this.mockMvc.perform(post("/users?username={username}&firstName={firstName}&lastName={lastName}&avatarId={avatarId}",
+                "test username", "test first name", "test last name", 0)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
 }
+
